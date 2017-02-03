f61eca34813324fa4e3325265b0dd7413f1257e4
VictorCORU <21500238@etu.unicaen.fr>
2017-02-03 10:35:17 +0100

    Modifications
    Ajout de fonctionnalité bootstrap et angular
    nouvelle feuille css

--- js/NoteController.js
+++ js/NoteController.js
@@ -2,35 +2,40 @@
  * Created by victor on 27/01/2017.
  */
 angular.module("NoteApp").controller("NoteController",function() {
-    this.messageNote= "";
+    this.messageNote = "";
     this.info = "";
     this.status = 1;
+    this.alert = "alert-info";
 
-    this.save = function() {
+
+    this.save = function () {
+        this.info = "Note sauvegardée";
 
     };
 
-    this.clear = function(){
+    this.clear = function () {
         this.messageNote = "";
+        this.info = "";
     };
 
-    this.count = function(){
-        var count =  100 - this.messageNote.length;
-        if(count <= 20 && count > 10)
+    this.count = function () {
+        var count = 100 - this.messageNote.length;
+        if (count <= 20 && count > 10) {
             this.status = 2;
-        else if(count <= 10 && count >0)
+            this.alert = "alert-warning";
+        }
+        else if (count <= 10 && count >= 0) {
             this.status = 3;
-        else
+            this.alert = "alert-danger";
+        }
+        else {
             this.status = 1;
+            this.alert = "alert-info";
+        }
 
+        this.info = "Note modifiée";
         return count;
 
     };
 
-
-})
-
-angular.module("NoteApp").controller("NoteController", function(){
-
-
-})
+});
